ff2ce56bd6999e9dc71ae1703a41207d8ae9fe2d
usheth <Dictionarypages1.1>
2019-05-24 11:04:33 -0700

    Added spring

---
+++ src/main/java/atmmachine/infrastructure/ATMMachineAPI.java
@@ -0,0 +1,15 @@
+package atmmachine.infrastructure;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ATMMachineAPI {
+
+    @RequestMapping("/hello")
+    public String hello(@RequestParam(value="name", defaultValue="World") String name) {
+        return "Hello";
+    }
+
+}
